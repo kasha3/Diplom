5a711ed62235d2d57f7939c093c3c99eb009f2b5
fadeev_alex <fadeevsasha3310@mail.ru>
2025-05-28 02:06:29 +0500

    Add docker-compose file

--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -77,11 +77,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
