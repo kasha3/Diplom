9bbb6a30dc2ce7eede91b9881d52b67070e39631
fadeev_alex <fadeevsasha3310@mail.ru>
2025-06-03 19:41:14 +0500

    Update Project.zip

--- Session5/app/src/main/java/com/example/session5/EventItem.java
+++
@@ -1,9 +0,0 @@
-package com.example.session5;
-
-public class EventItem {
-    public int id;
-    public String name;
-    public String date;
-    public String authorName;
-    public String description;
-}
